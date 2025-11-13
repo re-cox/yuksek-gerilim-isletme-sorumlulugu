33cee6a877553eb5004174d14efe3775fe11c6f0
re-cox <82539712+re-cox@users.noreply.github.com>
2025-11-13 13:51:37 +0300

    Add files via upload

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -100,13 +100,13 @@ const ContactForm: React.FC = () => {
             <h3 className="text-xl font-semibold text-white">Veya Doğrudan Bize Ulaşın</h3>
             <p className="mt-2 text-gray-400">Teklif formunu beklemek istemiyorsanız, bize aşağıdaki kanallardan ulaşabilirsiniz.</p>
             <div className="mt-6 flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-12">
-                <a href="tel:+902121234567" className="flex items-center text-lg text-yellow-400 hover:text-yellow-300 transition-colors">
+                <a href="tel:+905325729185" className="flex items-center text-lg text-yellow-400 hover:text-yellow-300 transition-colors">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" viewBox="0 0 20 20" fill="currentColor"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" /></svg>
-                    <span>+90 212 123 45 67</span>
+                    <span>+90 532 572 91 85</span>
                 </a>
-                <a href="mailto:info@ygsorumluluk.ist" className="flex items-center text-lg text-yellow-400 hover:text-yellow-300 transition-colors">
+                <a href="mailto:recep@aydemmuhendislik.com" className="flex items-center text-lg text-yellow-400 hover:text-yellow-300 transition-colors">
                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" viewBox="0 0 20 20" fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
-                    <span>info@ygsorumluluk.ist</span>
+                    <span>recep@aydemmuhendislik.com</span>
                 </a>
             </div>
         </div>
