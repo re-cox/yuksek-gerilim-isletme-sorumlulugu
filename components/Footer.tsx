2dac478f5897243da537cd6034727223d835369c
re-cox <82539712+re-cox@users.noreply.github.com>
2025-11-13 13:53:27 +0300

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,11 +52,11 @@ const Footer: React.FC = () => {
                   <ul className="mt-4 space-y-2">
                     <li className="flex items-center justify-center md:justify-start">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
-                        <a href="mailto:info@ygsorumluluk.ist" className="hover:text-white transition-colors">info@ygsorumluluk.ist</a>
+                        <a href="mailto:recep@aydemmuhendislik.com" className="hover:text-white transition-colors">recep@aydemmuhendislik.com</a>
                     </li>
                     <li className="flex items-center justify-center md:justify-start">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor"><path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" /></svg>
-                        <a href="tel:+902121234567" className="hover:text-white transition-colors">+90 212 123 45 67</a>
+                        <a href="tel:+905325729185" className="hover:text-white transition-colors">+90 532 572 91 85</a>
                     </li>
                      <li className="flex items-center justify-center md:justify-start">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /></svg>
